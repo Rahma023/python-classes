26850d981eaac9d40319c691f6a9f10e5b25fc6c
student <rahmafaizyusuf@gmail.com>
2021-05-31 19:47:55 +0300

    bank

--- bank.py
+++ bank.py
@@ -1,20 +1,33 @@
+from datetime import datetime
 class Account:
-    
     def __init__(self,name,phone_number):
         self.name=name
         self.phone_number=phone_number
 
         self.balance=0
+        self.statement=[]
 
     def show_balance(self):
       return f"Hello {self.name} your balance is {self.balance}"
 
-    def deposite(self,amount):
-      if amount > self.balance:
+    def deposit(self,amount):
+      if amount < self.balance:
          return f"Hello {self.name} your balance is {self.balance} "
       else:
-        self.balance-=amount
+        self.balance+=amount
+        now=datetime.now()
+        transaction={"amount":100, "time":now, "narration":"deposited"}
+        self.statement.append(transaction)
         return f"Hello {self.name} you cannot withdraw {self.show_balance()}"
+
+    def show_statement(self):
+      for transaction in self.statement:
+        amount=transaction["amount"]
+        narration=transaction["narration"]
+        time=transaction["time"]
+        date=time.strftime("%d/%m/%y")
+        print(f"{date} : {narration}  {amount} ")
+      return
 
     def withdraw(self,amount):
       if amount > self.balance:
@@ -22,13 +35,18 @@ class Account:
 
       else:
         self.balance-=amount
+        now=datetime.now()
+        withdrawal={"amount":200, "time":now, "narration":"deposited"}
+        self.statement.append(withdrawal)
+       
+
         return f"Hello {self.name} you have deposited {amount} Ksh you cannot withdraw {self.show_balance()} "
 
     def borrow(self,amount):
       return f"Hello {self.name} you have taken a loan of {amount} "
 
     def repay(self,amount):
-      return f"Hello {self.name} you have repaid a loan of {amount} "
+      return f"Hello {self.name} you have repaid a loan of {amount} "               
 
 
 
