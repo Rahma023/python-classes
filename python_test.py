5c2a81dc2de9e9db22a3034c4a28ca66245fb9af
student <rahmafaizyusuf@gmail.com>
2021-06-17 11:14:19 +0300

    assessment

--- python_test.py
+++ python_test.py
@@ -2,14 +2,12 @@ x = [100,110,120,130,140,150]
 z=[x*5 for x in x]
 print(z)
 
-x=range(1,30)
-for x in x:
-    if x%3==0:
-        print(x)
-    elif x%3!=0:
-        print("not divisible")
-    else:
-        print("not divisible")
+def divisible_by_three(n):
+    x=range(1,n)
+    for x in x:
+        if x%3==0:
+            print(x)
+    divisible_by_three(30)
 
 
 x = [[1,2],[3,4],[5,6]]
@@ -22,15 +20,14 @@ x = ['a','b','a','e','d','b','c','e','f','g','h']
 print(type(x))
 
   
-class Student:
-    def __init__(self, name ,age):
-        self.nam=name
-        self.age=age
-
-    def greet(self, name, age):
-        year=2021-age
-        return f"Hello {self.name} you are born in the year {self.age} "
-  
+def greet():
+    students = [{"age": 19, "name": "Eunice"},
+     {"age": 21, "name": "Agnes"},
+     {"age": 18, "name": "Teresa"}, 
+     {"age": 22, "name": "Asha"}]
+    for student in students:
+        print("Hello {} you were born in year {}".format(student["name"],2021-student["age"]))
+greet()
     
 
 
